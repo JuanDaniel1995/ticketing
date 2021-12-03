a1de181fa354dede272bc88271c6fc07ca1d31d8
Juan Daniel Vargas Cordero <juandanielvargascordero@gmail.com>
2021-12-03 15:04:48 -0600

    required fixes for prod environment

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const buildClient = ({ req }) => {
   if (typeof window === 'undefined') {
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.ticketing-app.site/',
       headers: req.headers
     });
   } else {
